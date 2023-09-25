df7720cf68d0603b721f78accead6b7cbaca91ad
PhantasiaOwO <pjy030513@live.com>
2023-09-25 20:25:02 +0800

    feat: Expression class interfaces

---
+++ EquationGenerator/Expression.cpp
@@ -0,0 +1,11 @@
+﻿#include "Expression.h"
+
+Expression::Expression() {}
+
+Expression::Expression(int maxNum, int depth) {}
+
+Expression::Expression(string& expression) {}
+
+std::string Expression::GetString() const {}
+
+std::Fraction Expression::GetResult() const {}
