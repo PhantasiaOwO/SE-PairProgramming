8d308e6f39d0167f10e16e4f07a73b46fcede964
PhantasiaOwO <pjy030513@live.com>
2023-09-26 15:55:04 +0800

    feat: Functions interfaces

---
+++ EquationGenerator/Functions.h
@@ -0,0 +1,12 @@
+﻿#ifndef __FUNCTIONS__
+#define __FUNCTIONS__
+
+#include <string>
+
+// 生成表达式功能函数主体
+void GenerateExpression(const int exerciseCount, const int maxRandomNum);
+
+// 判断答案函数主体
+void JudgeAnswers(const std::string& exerciseFileName, const std::string& answersFileName);
+
+#endif // __FUNCTIONS__
