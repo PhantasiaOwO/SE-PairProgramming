4a48b689802094ca425874f02c1ff4366b33e0ff
PhantasiaOwO <pjy030513@live.com>
2023-09-26 15:45:30 +0800

    comment: FileIOModule comments

--- EquationGenerator/FileIOModule.h
+++ EquationGenerator/FileIOModule.h
@@ -11,9 +11,13 @@ const std::string DivideSign = "÷";
 const std::string ExercisesFileName = "Exercises.txt";
 const std::string AnswersFileName = "Answers.txt";
 
+// 向文件写入习题列表，传入参数：习题列表
 void WriteExercisesToFile(const std::vector<Expression>& exercises);
+// 向文件写入答案列表，传入参数：习题列表
 void WriteAnswersToFile(const std::vector<Fraction>& results);
+// 从文件读取习题列表，传入参数：文件名称、习题列表引用
 void ReadExercisesFromFile(const string fileName, std::vector<Expression>& outExpressions);
+// 从文件读取答案列表，传入参数：文件名称、习题列表引用
 void ReadAnswersFromFile(const string fileName, std::vector<Fraction>& outFraction);
 
 #endif // __FILEIOMODULE__
